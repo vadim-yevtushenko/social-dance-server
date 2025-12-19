bba958a3d4020ed9ce51ad83e41cbf14ea170b79
Vadim Yevtushenko <vadik.chicoto@gmail.com>
2025-12-19 18:33:36 +0200

    updated EmailServiceImpl and added timeouts

--- src/main/java/com/example/socialdanceserver/service/impl/EmailServiceImpl.java
+++ src/main/java/com/example/socialdanceserver/service/impl/EmailServiceImpl.java
@@ -4,44 +4,46 @@ import com.example.socialdanceserver.service.EmailService;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class EmailServiceImpl extends BaseService implements EmailService {
 
-    @Autowired
     private JavaMailSender emailSender;
 
     @Async
-    @SneakyThrows
     @Override
     public void sendEmails(List<InternetAddress> toAddresses, String subject, String text) {
-        MimeMessage message = emailSender.createMimeMessage();
 
-        MimeMessageHelper helper = new MimeMessageHelper(message, false);
+        try {
+            MimeMessage message = emailSender.createMimeMessage();
 
-        helper.setSubject(subject);
+            MimeMessageHelper helper = new MimeMessageHelper(message, false);
+            helper.setSubject(subject);
 
-        InternetAddress fromAddress = new InternetAddress("socialdance214@gmail.com", "Social Dances Webapp");
-        helper.setFrom(fromAddress);
+            InternetAddress fromAddress = new InternetAddress("socialdance214@gmail.com", "Social Dances Webapp");
+            helper.setFrom(fromAddress);
 
-        helper.setTo(convertToArray(toAddresses));
+            helper.setTo(convertToArray(toAddresses));
 
-        if (text != null) {
-            message.setContent(text, "text/html; charset=utf-8");
+            if (text != null) {
+                message.setContent(text, "text/html; charset=utf-8");
 //            helper.setText(text, true);
-        } else {
-            helper.setText("", false);
-        }
+            } else {
+                helper.setText("", false);
+            }
 
-        emailSender.send(message);
+            emailSender.send(message);
+        } catch (Exception e) {
+            log.warn("Email sending failed: {}", e.getMessage());
+        }
     }
 
     private InternetAddress[] convertToArray(List<InternetAddress> toAddresses) {
